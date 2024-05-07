d8bb84f4816d80ec2c0c13488761878eb3f1322b
Kaddesh <oludamolaijimade@gmail.com>
2024-05-07 05:31:03 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HeadlineCards from './components/HeadlineCards'
+import Food from './components/Food'
+import Category from './components/Category'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+    </div>
+  )
+}
+
+export default App
